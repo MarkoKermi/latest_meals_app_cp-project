902a46cd25729f88e67a8f1979c87123f53e92cf
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-02-10 00:33:46 +0300

    test count likes function

--- src/test/countest.test.js
+++ src/test/countest.test.js
@@ -1,4 +1,8 @@
-import { countProducts, sumComment } from "../modules/displayItems.js";
+import {
+  countProducts,
+  sumComment,
+  CountLike,
+} from "../modules/displayItems.js";
 
 describe("Comments and items count Test", () => {
   test("Count Items", () => {
@@ -11,5 +15,12 @@ describe("Comments and items count Test", () => {
     const array = ["item1", "item2", "item3"];
     sumComment(array);
     expect(sumComment(array)).toEqual("Total Comments(3)");
+  });
+  test("Count likes", () => {
+    document.body.innerHTML =
+      "<div>" + '<b id="1" class="likes-counter">2</b>' + "</div>";
+    CountLike([{ likes: 5, item_id: "1" }]);
+    const item = document.querySelector(".likes-counter").innerHTML;
+    expect(+item).toEqual(5);
   });
 });
