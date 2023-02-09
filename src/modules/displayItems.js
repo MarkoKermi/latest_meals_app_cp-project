2fd6d3946c8dd9f5ab903bb56a2e961d04afb090
Marko Kermiciev <kermicievmarko@gmail.com>
2023-02-09 20:53:09 +0100

    popup window, popup comments & display likes setup

--- src/modules/displayItems.js
+++ src/modules/displayItems.js
@@ -1,3 +1,5 @@
+import { modalWindow, Cards, ProductsCounter } from "./display.js";
+
 function DisplayCards(data) {
   document.querySelector(".cards").innerHTML = "";
   data.forEach((element) => {
@@ -31,3 +33,125 @@ function DisplayCards(data) {
           `;
   });
 }
+
+function DisplayPopup(data, examples) {
+  modalWindow.innerHTML = ` <div class="closeBtn">X</div>
+  <div class="popup">
+            <div class="popup-header" >
+                
+                    <div class="img-popcontainer">
+                        <div class="img-pophold">
+                            <div class="img">
+                                <img src="${data.strCategoryThumb}">
+                            </div>
+                        </div>
+                    </div>
+                <div class="popup-title">${data.strCategory}</div>
+            </div>
+
+            <div class="popup-details">
+              
+                <div class="details">
+                    <div class="images">
+                    </div>
+                    <hr />
+                   <div class="details-text">
+                   ${data.strCategoryDescription}
+                    </div>
+                </div>
+                <div class="popup-comments">
+                    <div class="container-comments">
+                        <form class="form">
+                    
+                    <textarea id="comment" type="text" placeholder="Leave comment" required/></textarea>
+                    <div class="form-header"><input required id="name" type="text" placeholder="Enter name" /> <button class="submit" id="${data.idCategory}">submit</button></div>
+                        </form>
+                        <div class="Comment-counter">
+                        Total Comments(0)
+                        </div>
+                     <hr/>
+                     
+                    <div class="comment-list">
+                        
+                        <ul class="comments">
+                                        
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </div>`;
+}
+const images = document.querySelector(".images");
+examples.meals.slice(0, 3).forEach((element) => {
+  const div = document.createElement("div");
+  div.classList.add("single-product-example");
+  div.innerHTML = ` 
+    <div class="img-popholdpop">
+        <div class="imgex">
+            <img src=${element.strMealThumb}>
+        </div>
+    </div>
+    <p class="title-example">${element.strMeal.substr(0, 15)}</p>
+`;
+  images.appendChild(div);
+});
+
+const closeBtn = document.querySelector(".closeBtn");
+
+closeBtn.addEventListener("click", () => {
+  modalWindow.style.display = "none";
+});
+
+function sumComment(data) {
+  const msg = `Total Comments(${data.length})`;
+  return msg;
+}
+
+function CountComment(data) {
+  const CommentCounter = document.querySelector(".Comment-counter");
+  CommentCounter.innerHTML = sumComment(data);
+}
+
+function DisplayComments(data) {
+  const commentSection = document.querySelector(".comments");
+  let comment = "";
+  data.forEach((item) => {
+    comment += `<li class="single-comment">
+      
+      <b class="user-comment">${item.username}: ${item.comment}</b>
+      </li>`;
+  });
+  commentSection.innerHTML = comment;
+  CountComment(data);
+}
+
+function countProducts(data) {
+  return data.length;
+}
+
+function Counter(data) {
+  ProductsCounter.innerHTML = countProducts(data);
+}
+
+function CountLike(data) {
+  const likeCount = document.querySelectorAll(".likes-counter");
+  likeCount.forEach((item) => {
+    data.forEach((likes) => {
+      if (item.id === likes.item_id) {
+        item.innerHTML = likes.likes;
+      }
+    });
+  });
+}
+
+export {
+  DisplayCards,
+  DisplayPopup,
+  Counter,
+  DisplayComments,
+  CountComment,
+  CountLike,
+  sumComment,
+  countProducts,
+};
